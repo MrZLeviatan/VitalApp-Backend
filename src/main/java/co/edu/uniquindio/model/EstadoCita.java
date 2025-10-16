e901d180ad903c8f7a7f5318052d7807072b410b
Nicolas C.S <117557162+MrZLeviatan@users.noreply.github.com>
2025-10-16 01:05:33 -0500

    Corrección de problemas
    Se corrigen problemas relacionados al despliegue

--- src/main/java/co/edu/uniquindio/model/EstadoCita.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.model;
-
-public enum EstadoCita {
-    AGENDADA,
-    EN_REVISION,
-    CANCELADA,
-    COMPLETADA,
-}
--- src/main/java/co/edu/uniquindio/model/TipoExamen.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uniquindio.model;
-public enum TipoExamen {
-    SANGRE,
-    IMAGENES,
-    ORINA,
-    OTRO,
-}
